8d70beb3cbf1ce519e8d7f53e1d0cb5112c5bcf2
Perry <chenzhipeng901105@gmail.com>
2015-07-29 12:52:33 +0800

    add code

---
+++ SwiftProjectDemo/EmployeeDataProviderKit/BaseApiResponseModel.swift
@@ -0,0 +1,9 @@
+//
+//  BaseApiResponseModel.swift
+//  SwiftProjectDemo
+//
+//  Created by PerryChen on 7/29/15.
+//  Copyright (c) 2015 PerryChen. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftProjectDemo/EmployeeDataProviderKit/EmployeeDataProviderKit.h
@@ -0,0 +1,19 @@
+//
+//  EmployeeDataProviderKit.h
+//  EmployeeDataProviderKit
+//
+//  Created by PerryChen on 7/29/15.
+//  Copyright (c) 2015 PerryChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmployeeDataProviderKit.
+FOUNDATION_EXPORT double EmployeeDataProviderKitVersionNumber;
+
+//! Project version string for EmployeeDataProviderKit.
+FOUNDATION_EXPORT const unsigned char EmployeeDataProviderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmployeeDataProviderKit/PublicHeader.h>
+
+
